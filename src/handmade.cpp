ddcc097a65874275d364fd84a70d1d8f32dcf225
Tyler Funk <tfunk@fastmail.fm>
2015-01-23 11:31:06 -0600

    Halfway through 002
    Small changes to shell.bat, including removing the virtual drive
    thing...seems unnecessary for my setup.
    
    We now have a window, yay!

--- src/handmade.cpp
+++ src/handmade.cpp
@@ -1,13 +1,99 @@
 #include <windows.h>
 
-int CALLBACK
-WinMain(HINSTANCE hInstance,
-        HINSTANCE hPrevInstance,
-        LPSTR lpCmdLine,
-        int nCmdShow)
+
+LRESULT CALLBACK
+MainWindowCallback(HWND Window,
+                   UINT Message,
+                   WPARAM WParam,
+                   LPARAM LParam)
 {
-    MessageBox(0, "This is Handmade Hero!", "Handmade Hero",
-            MB_OK|MB_ICONINFORMATION);
+    LRESULT Result = 0;
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVEAPP\n");
+        } break;
+        default:
+        {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+            break;
+        }
+    }
+    return(Result);
+}
+
+int CALLBACK
+WinMain(HINSTANCE Instance,
+        HINSTANCE PrevInstance,
+        LPSTR CommandLine,
+        int ShowCode)
+{
+    WNDCLASS WindowClass = {};
+
+    // Define parts of Window class
+    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    //WindowClass.hIcon;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    // Register window class
+    if (RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle =
+            CreateWindowEx(
+              0,
+              WindowClass.lpszClassName,
+              "Handmade Hero",
+              WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+              CW_USEDEFAULT,
+              CW_USEDEFAULT,
+              CW_USEDEFAULT,
+              CW_USEDEFAULT,
+              0,
+              0,
+              Instance,
+              0);
+        if (WindowHandle)
+        {
+            MSG Message;
+            for(;;)
+            {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0)
+                {
+                    // Translate and Dispatch message
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // TODO(tyler): Logging
+        }
+    }
+    else
+    {
+        // TODO(tyler): Logging
+    }
 
     return(0);
 }
